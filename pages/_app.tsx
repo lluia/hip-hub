b93b40bdc9198f9c90351dfbe5ad6f5e52adf59f
Lluis Agusti <ll@llu.lu>
2020-05-10 18:26:57 +0200

    chore(linting): fix configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <div style={{ backgroundColor: 'whitesmoke' }}>
       <Component {...pageProps} />
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function HomePage() {
+function About() {
   return <div>Welcome to Next.js!</div>
 }
 
-export default HomePage
+export default About
