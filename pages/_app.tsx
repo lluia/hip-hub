737a2328ed6651491fa73f4f950470b717990cd2
Lluis Agusti <ll@llu.lu>
2020-05-20 16:35:56 +0200

    feat(app): add dummy navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { AppProps } from 'next/app'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div style={{ backgroundColor: 'whitesmoke' }}>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
       <Component {...pageProps} />
     </div>
   )
