4651c92e6a146b1f8f92aa9b7c495e4a89fa9ed1
Lluis Agusti <ll@llu.lu>
2020-05-26 14:48:45 +0200

    chore(eslint): add 'rules of hooks' plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,32 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: true,
+        },
+      },
+    ],
   },
   parserOptions: {
     ecmaFeatures: {
@@ -24,4 +38,4 @@ module.exports = {
       version: 'detect',
     },
   },
-};
+}
