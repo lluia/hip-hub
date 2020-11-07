066f5b23ebc1344df89583a8dc1c415cb671d784
Lluis Agusti <hi@llu.lu>
2020-11-07 15:59:38 +0100

    test(feed): test empty notifications

--- cypress/integration/feed.test.js
+++ cypress/integration/feed.test.js
@@ -1,22 +1,40 @@
-import notifications from '../fixtures/notifications.json'
+import notifications from '../fixtures/notifications-full.json'
 import { notificationsMock } from '../../test/server/mocks/notifications'
 
-context('• Login', () => {
-  before(() => {
-    cy.route2('api/notifications', {
-      fixture: 'notifications.json',
-    }).visitLoggedIn('/')
+context('• Feed', () => {
+  describe('– with notifications', () => {
+    before(() => {
+      cy.route2('api/notifications', {
+        fixture: 'notifications-full.json',
+      }).visitLoggedIn('/')
+    })
+
+    it('displays fetched notifications', () => {
+      cy.findByTestId('NOTIFICATION_FEED').within(() => {
+        notifications.forEach(({ subject, repository, id }) => {
+          cy.findByTestId(`NOTIFICATION_CARD-${id}`).within(() => {
+            cy.findByText(subject.title)
+              .findByText(`${repository.owner.login} / ${repository.name}`)
+              .getNotificationLabel(subject.type)
+              .then((label) => {
+                cy.findByText(label)
+              })
+          })
+        })
+      })
+    })
   })
 
-  it('displays fetched notifications', () => {
-    notifications.forEach(({ subject, repository, id }) => {
-      cy.findByTestId(`NOTIFICATION_CARD-${id}`).within(() => {
-        cy.findByText(subject.title)
-          .findByText(`${repository.owner.login} / ${repository.name}`)
-          .getNotificationLabel(subject.type)
-          .then((label) => {
-            cy.findByText(label)
-          })
+  describe('- without notifications', () => {
+    before(() => {
+      cy.route2('api/notifications', {
+        fixture: 'notifications-empty.json',
+      }).visitLoggedIn('/')
+    })
+
+    it("displays a context message when there's no notifications", () => {
+      cy.findByTestId('NOTIFICATION_FEED').within(() => {
+        cy.findByText("You don't have any new notifications!")
       })
     })
   })
